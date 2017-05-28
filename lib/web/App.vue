8b34bf1e2c3b3816ce14eff66a0f155d9f663826
Yuriy Nemtsov <nemtsov@gmail.com>
2017-05-28 01:13:43 -0400

    vue ssr

---
+++ lib/web/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <header>
+      <span>SP App</span>
+    </header>
+
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ lib/web/components/Hello.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+    <div>{{ item.title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+
+  asyncData({ store, route }) {
+    return store.dispatch('fetchItem', route.params.id);
+  },
+
+  data() {
+    return {
+      msg: 'Hello, amazing world!',
+    };
+  },
+
+  computed: {
+    item () {
+      return this.$store.state.items[this.$route.params.id];
+    }
+  }
+};
+</script>
+
+<style scoped>
+.hello {
+  font-size: 120%;
+}
+</style>
