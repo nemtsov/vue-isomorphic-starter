8de86b1cfc70c01a42591c111b122bb4c7e82c85
Yuriy Nemtsov <nemtsov@gmail.com>
2017-06-27 01:42:04 -0400

    authentication

---
+++ lib/web/app/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="root" style="width: 280px; margin: 40px auto; padding: 10px;"></div>
+  </div>
+</template>
+
+<script>
+import auth0 from 'auth0-js';
+
+export default {
+  name: 'login',
+
+  mounted() {
+    const { env } = this.$store.state;
+
+    const auth = new auth0.WebAuth({
+      domain: env.AUTH0_DOMAIN,
+      clientID: env.AUTH0_CLIENT_ID,
+      redirectUri: env.AUTH0_CALLBACK_URL,
+      responseType: 'code',
+      scope: 'openid'
+    });
+
+    auth.authorize();
+  },
+};
+</script>
+
+<style>
+</style>
